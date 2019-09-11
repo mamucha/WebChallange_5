f6ad1e38e0d06ace6257a0c5c63e0d73919d5567
Marcin <marcin9213@gmail.com>
2019-09-11 21:48:00 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+console.log("czesc");
+const btn = document.querySelector('button');
+
+let number = 1;
+let activeNumber = 1;
+console.log("czesc");
+
+const addElement = function () {
+    const div = document.createElement('div');
+    div.textContent = number;
+
+    if (number % 5 == 0) {
+        div.classList.add('circle')
+    }
+
+    document.body.appendChild(div);
+    number++;
+    console.log(number);
+    btn.addEventListener("click", addElement);
+}
