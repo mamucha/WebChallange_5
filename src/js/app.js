1b5b30fb85addaf8587e62d63e3f351e402a8931
Marcin <marcin9213@gmail.com>
2019-09-28 12:42:24 +0200

    include google maps

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,21 @@
-console.log("czesc");
-const btn = document.querySelector('button');
+// console.log("czesc");
+// const btn = document.querySelector('button');
 
-let number = 1;
-let activeNumber = 1;
-console.log("czesc");
+// let number = 1;
+// let activeNumber = 1;
+// console.log("czesc");
 
-const addElement = function () {
-    const div = document.createElement('div');
-    div.textContent = number;
+// const addElement = function () {
+//     const div = document.createElement('div');
+//     div.textContent = number;
 
-    if (number % 5 == 0) {
-        div.classList.add('circle')
-    }
+//     if (number % 5 == 0) {
+//         div.classList.add('circle')
+//     }
 
-    document.body.appendChild(div);
-    number++;
-    console.log(number);
-    btn.addEventListener("click", addElement);
-}
+//     document.body.appendChild(div);
+//     number++;
+//     console.log(number);
+//     btn.addEventListener("click", addElement);
+// }
+require('../scss/components/c-map/c-map.js');
---
+++ src/scss/components/c-map/c-map.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var uluru = {
+        lat: 50.211485,
+        lng: -5.481904
+    };
+    var map = new google.maps.Map(document.getElementById('c-map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
