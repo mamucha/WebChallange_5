cb999a5e5911143c3e8a81b7241bd87db931a22c
Marcin <marcin9213@gmail.com>
2019-10-02 22:16:53 +0200

    correct display google maps. Assigning a function to a variable

--- src/scss/components/c-map/c-map.js
+++ src/scss/components/c-map/c-map.js
@@ -1,8 +1,10 @@
-function initMap() {
+const initMap = () => {
     var uluru = {
         lat: 50.211485,
         lng: -5.481904
     };
+
+
     var map = new google.maps.Map(document.getElementById('c-map'), {
         zoom: 15,
         center: uluru
@@ -12,3 +14,5 @@ function initMap() {
         map: map
     });
 }
+
+initMap();
