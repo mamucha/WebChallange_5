a4e24ca147bd10487880a7ac98c331f66c3f94e8
Marcin <marcin9213@gmail.com>
2019-10-12 12:04:45 +0200

    add scroll links to nav menu

--- src/scss/components/c-menu/c-menu.js
+++ src/scss/components/c-menu/c-menu.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
         $(this).toggleClass('c-menu__hamburger--open');
         $('.c-menu__list').slideToggle();
     });
+
+    $('.c-menu__link').on('click', function () {
+        const goToSection = $(this).attr('href');
+
+        console.log(goToSection);
+
+        $('body, html').animate({
+            scrollTop: $(goToSection).offset().top
+        }, '3000');
+    })
+
 });
