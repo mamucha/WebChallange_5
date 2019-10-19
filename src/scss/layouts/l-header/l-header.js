cf9b8f6723021a0cd40be58bf066b992da508eb5
Marcin <marcin9213@gmail.com>
2019-10-19 23:56:23 +0200

    fix sticky nav

--- src/scss/layouts/l-header/l-header.js
+++ src/scss/layouts/l-header/l-header.js
@@ -1,8 +1,9 @@
 $(document).scroll(function () {
     const scrollValue = $(this).scrollTop();
+    const windowWidth = $(window).width();
     let header = $('.l-header__wrapper');
 
-    if (scrollValue > 100) {
+    if (scrollValue > 100 && windowWidth > 1024) {
         header.addClass('l-header__wrapper--sticky');
     } else {
         header.removeClass('l-header__wrapper--sticky')
