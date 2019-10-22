b163bbef1f01980c697ff89a03ec1b87b5cf9081
Marcin <marcin9213@gmail.com>
2019-10-22 19:04:47 +0200

    add new image

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,6 @@ require('../scss/components/c-menu/c-menu.js');
 require('../scss/components/c-opinion/c-opinion.js');
 require('../scss/components/c-back-top/c-back-top.js');
 require('../scss/components/c-gallery/c-gallery.js');
+require('../scss/components/c-card/c-card.js');
 require('../scss/layouts/l-wrapper/l-wrapper.js');
 require('../scss/layouts/l-header/l-header.js');
